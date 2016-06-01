10f28c736a5ed017acc3c01af4e2362770621793
micEnigma <mic.excel@gmail.com>
2016-06-01 12:13:14 +0100

    Update chartApp.js

--- chartApp.js
+++ chartApp.js
@@ -39,13 +39,12 @@ function enterData() {
         document.getElementById('feedback').innerHTML = lastEntered;       
     }
 }    
-    
 
 // Delete the last data entered to correct errors
 
-function delData() {
-    let approveDel = confirm('Are you sure you want to delete the last data entered?')
-    if(approveDel) {
+function delData(approve) {
+    approve = confirm('Are you sure you want to delete the last data entered?')
+    if(approve) {
         valueTotal -= chartValues[chartValues.length-1]
         chartLabels.pop();
         chartValues.pop();
@@ -56,7 +55,7 @@ function delData() {
 
 // Accept user command to draw
 
-function drawSelected(){
+function drawSelected(approveChart){
     if (chartValues.length === 0) {
         alert('You have not entered any values yet, please enter some values and try again.')
     }
@@ -70,8 +69,8 @@ function drawSelected(){
                 break;
             }
         }
-        let approveChartDraw = confirm('You are about to draw a ' + radios[i].value + '. Click OK to continue or cancel to choose another chart type.');
-        if (approveChartDraw){
+        approveChart = confirm('You are about to draw a ' + radios[i].value + '. Click OK to continue or cancel to choose another chart type.');
+        if (approveChart){
             draw(chartDiagram);
             // reset the arrays after drawing
             chartLabels = [];
@@ -126,19 +125,21 @@ function pieChart(arr1,arr2){
         pieEnd += arr2[i];        
         
         // this changes the colour of the chart sectors from red through to blue
-        appCtx.fillStyle = 'rgba(' + Math.floor(128-128*i/arr2.length) +',' + Math.floor(192*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',0.8)';
+        appCtx.fillStyle = 'rgba(' + 127*(Math.floor(i+1)%3) +',' + 127*(Math.floor((i+1)/9)%3) + ',' + 127*(Math.floor((i+1)/3)%3) + ',.8)';
 
         appCtx.beginPath();
         appCtx.arc(100, 75, 60, pieStart, pieEnd);
         appCtx.lineTo(100,75);
+        //appCtx.stroke();
         appCtx.fill();
 
         appCtx.font = '10px arial';        
+        appCtx.fillText(arr1[i], 200, 50+15*i);  
+        appCtx.fillText(chartValues[i], 250, 50+15*i);
+        appCtx.fillStyle = 'black';        
         appCtx.fillText('Legend by colour: ', 200,20);
         appCtx.fillText('Label', 200, 35); 
-        appCtx.fillText('Value', 250, 35);  
-        appCtx.fillText(arr1[i], 200, 50+15*i);  
-        appCtx.fillText(chartValues[i], 250, 50+15*i);               
+        appCtx.fillText('Value', 250, 35);             
     };
 }
 
@@ -167,7 +168,7 @@ function xYAxis() {
 function histogram(arr1, arr2){     
     xYAxis();        
     for(i=0; i<arr2.length; i++){
-        appCtx.fillStyle = 'rgba(' + Math.floor(255-128*i/arr2.length) +',' + Math.floor(128*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',1)';  
+        appCtx.fillStyle = 'rgba(' + 127*(Math.floor((i+1)/3)%3) +',' + 127*(Math.floor((i+1)/9)%3) + ',' + 127*(Math.floor(i+1)%3) + ',1)';  
         appCtx.fillRect(45+40*i,120-normalize(i, arr2),40,normalize(i, arr2));        
         appCtx.font = '7px arial';
         appCtx.fillText(arr1[i], 45+40*i,130);
@@ -181,7 +182,7 @@ function histogram(arr1, arr2){
 function barChart(arr1, arr2){
     xYAxis();    
     for(i=0; i<arr2.length; i++){
-        appCtx.fillStyle = 'rgba(' + Math.floor(255-128*i/arr2.length) +',' + Math.floor(128*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',1)';   
+        appCtx.fillStyle = 'rgba(' + 127*(Math.floor((i+1)/3)%3) +',' + 127*(Math.floor((i+1)/9)%3) + ',' + 127*(Math.floor(i+1)%3) + ',1)';   
         appCtx.fillRect(55+45*i,120-normalize(i, arr2),30,normalize(i, arr2));
         appCtx.font = '7px arial';
         appCtx.fillText(arr1[i], 55+45*i,130);
@@ -210,13 +211,10 @@ function lineChart(arr1, arr2){
 function tableHeading() {
     appCtx.beginPath();
     appCtx.strokeRect(45, 0, 120, 15);
-
     appCtx.font = '10px arial';
     appCtx.fillText('Data Labels', 65, 10);
-
     appCtx.strokeRect(165, 0, 120, 15);
     appCtx.fillText('Data Values', 185, 10);
-
     appCtx.strokeStyle = 'black';       
 }
 
@@ -232,4 +230,13 @@ function tableChart(arr1, arr2){
         appCtx.fillText(arr1[i], 65,25+15*i);
         appCtx.fillText(arr2[i], 185,25+15*i);
     }
+}
+
+//Clear all charts on Canvas to start new drawing
+
+function clearCanvas(approve) {
+    approve = confirm('Are you sure you want to clear all drawings and start again?');
+    if(approve) {
+        appCtx.clearRect(0, 0, 300, 150);
+    }    
 }
